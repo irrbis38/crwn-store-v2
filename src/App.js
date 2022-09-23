eb3590e6f396abd3261ae0a26698b6cc37ed6eb0
Vasily Ilyuhin <v.ilyuhin@gmail.com>
2022-09-23 14:23:59 +0300

    feat: add signIn component and add firebase

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/routes/home/home.components";
 import Navigation from "./components/routes/navigation/navigation.component";
+import SignIn from "./components/routes/sign-in/sign-in.components";
 
 const Shop = () => <h1>I am the shop page</h1>;
 
@@ -10,6 +11,7 @@ const App = () => {
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
+        <Route path="sign-in" element={<SignIn />} />
       </Route>
     </Routes>
   );
---
+++ src/components/routes/sign-in/sign-in.components.jsx
@@ -0,0 +1,5 @@
+const SignIn = () => {
+  return <h1>This is signIn page</h1>;
+};
+
+export default SignIn;
