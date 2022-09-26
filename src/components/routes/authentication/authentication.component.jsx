55d30903a1ed036b4620bf30fb13a662102afa14
Vasily Ilyuhin <v.ilyuhin@gmail.com>
2022-09-26 17:43:00 +0300

    feat: complete authentication page

--- src/components/routes/authentication/authentication.component.jsx
+++ src/components/routes/authentication/authentication.component.jsx
@@ -1,20 +1,12 @@
-import {
-  signInWithGooglePopup,
-  createUserDocumentFromAuth,
-} from "../../../utils/firebase/firebase.utils";
+import SignUpForm from "../../sign-up-form/sign-up.component";
+import SignInForm from "../../sign-in-form/sign-in-form.component";
 
-import SignUpForm from "../../sign-up-form/sign-up.compooent";
+import "./authentication.styles.scss";
 
 const Authentication = () => {
-  const logGoogleUser = async () => {
-    const { user } = await signInWithGooglePopup();
-    const userDocRef = await createUserDocumentFromAuth(user);
-  };
-
   return (
-    <div>
-      <h1>Sign In Page</h1>
-      <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+    <div className="authentication-container">
+      <SignInForm />
       <SignUpForm />
     </div>
   );
