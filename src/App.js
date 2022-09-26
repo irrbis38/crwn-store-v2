9c06728ae3e777aad21af8831ba9d26b1f40e400
Vasily Ilyuhin <v.ilyuhin@gmail.com>
2022-09-26 16:18:10 +0300

    feat: add sign-up component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/routes/home/home.components";
 import Navigation from "./components/routes/navigation/navigation.component";
-import SignIn from "./components/routes/sign-in/sign-in.components";
+import SignIn from "./components/routes/sign-in/sign-in.component";
 
 const Shop = () => <h1>I am the shop page</h1>;
 
