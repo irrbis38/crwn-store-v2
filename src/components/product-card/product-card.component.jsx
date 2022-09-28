61227ac2e0955d576a3836769aed9f30a393d4f0
Vasily Ilyuhin <v.ilyuhin@gmail.com>
2022-09-28 16:46:46 +0300

    feat: implement add to cart logic

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,17 +1,27 @@
-import "./product-card.styles.scss";
+import { useContext } from "react";
+
+import { CartContext } from "../../contexts/cart.context";
 
 import Button from "../button/button.component";
 
+import "./product-card.styles.scss";
+
 const ProductCard = ({ product }) => {
-  const { title, price, imageUrl } = product;
+  const { name: title, price, imageUrl } = product;
+  const { addItemToCart } = useContext(CartContext);
+
+  const addProductToCart = () => addItemToCart(product);
+
   return (
     <div className="product-card-container">
-      <img src={imageUrl} alt={title} />
+      <img src={imageUrl} alt={`${title}`} />
       <div className="footer">
         <span className="name">{title}</span>
         <span className="price">{price}</span>
       </div>
-      <Button buttonType="inverted">Add to card</Button>
+      <Button buttonType="inverted" onClick={addProductToCart}>
+        Add to card
+      </Button>
     </div>
   );
 };
